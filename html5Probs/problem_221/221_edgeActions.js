ef64716fd5c1f9ec914e294e0df7143fb602d53e
jnewman <jnewman@cs.umass.edu>
2014-11-06 13:42:26 -0500

    Updated problems 214, 221 and 830 to template 5.

--- html5Probs/problem_221/221_edgeActions.js
+++ html5Probs/problem_221/221_edgeActions.js
@@ -77,6 +77,7 @@ var Composition = Edge.Composition, Symbol = Edge.Symbol; // aliases for commonl
          );
          
          function init() {
+            probUtilsInit(sym);
          	console.log("Loaded libraries successfully");
          }
 
